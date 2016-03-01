88f0cbdda1a19181a85a97917d30e2ef021d18f5
고동균 <itskooh@Kerson-iMac.local>
2016-03-01 23:40:17 +0900

    oop3,4 추가

---
+++ OOP/oop4.js
@@ -0,0 +1,31 @@
+/*
+ *  객체지향 프로그래밍
+ *	    1. 클래스, 생성자, 메서드
+ *			2) 상속
+ */
+
+Function.prototype.method = function ( methodName, func ) {
+    if ( !this.prototype[methodName] ) {
+        this.prototype[methodName] = func;
+    }
+};
+
+function Person ( name ) {
+    this.name = name;
+}
+
+Person.method ( 'getName', function() {
+   return this.name;
+});
+
+Person.method ( 'setName', function ( name ) {
+    this.name = name;
+});
+
+var you = new Person ( 'zzoon' );
+console.dir ( you );
+console.log ( you.getName() );
+
+var me = new Person ( 'merry' );
+console.log ( me.getName() );
+console.dir ( me );  
